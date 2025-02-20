ee8f7dec51c73df39f89b8e8867c153a0a648970
wassim-oucouc <wassimoucouc03@gmail.com>
2025-02-20 15:15:17 +0100

    Update Database.php

--- app/core/db/Database.php
+++ app/core/db/Database.php
@@ -1,41 +1,26 @@
 <?php
-namespace app\core\db;
 
+class Connection
+{
+    private $dsn = "mysql:host=localhost;dbname=youdemy;charset=UTF8";
+    private $username = 'root';
+    private $password;
 
-class Database {
-    private static $servername = "localhost";
-    private static $port = "5555";
-    private static $username= "postgres";
-    private static $password = "123123";
-    private static $dbname = "sokoworksxdb";
-    private static $connexion;
-    private static $instance;
-   
-    private function __construct(){
-        if (!self::$connexion) {
-            try {
-                self::$connexion = new PDO("pgsql:host=".self::$servername.";port=".self::$port." dbname=".self::$dbname.";",self::$username,self::$password);
-                self::$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo("connexion done with no issues");
-            } catch (PDOException $e) {
-                echo'Error:'.$e;
-            }
+    public function connection()
+    {
+        try
+        {
+            $conn = new PDO($this->dsn,$this->username,$this->password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $conn;
         }
-        
+        catch(PDOException $error)
+        {
+            throw new Exception($error->getmessage());
+        }
     }
-
-    public static function getInstance(){
-        if(!self::$instance){
-            self::$instance = new Database();
-        }
-            return self::$instance ;
-        }
-        
-        public function getConnection(){
-            return self::$connexion;
-        }
-
-       
 }
 
-Database::getInstance()->getConnection();
+
+
+?>
